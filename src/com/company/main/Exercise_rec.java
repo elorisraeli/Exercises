cc43fc33d2352d4c44b36ea319410eb42d2a3462
elorisraeli <53333654+elorisraeli@users.noreply.github.com>
2021-12-10 13:22:19 +0200

    Recursion exercise

--- src/com/company/main/Exercise_rec.java
+++ src/com/company/main/Exercise_rec.java
@@ -3,31 +3,41 @@ package com.company.main;
 public class Exercise_rec {
     // Function to print all the permutations of str
     static void printPermutn(String str, String ans) {
-
         // If string is empty
         if (str.length() == 0) {
             System.out.print(ans + " ");
             return;
         }
-
         for (int i = 0; i < str.length(); i++) {
-
             // ith character of str
             char ch = str.charAt(i);
-
             // Rest of the string after excluding
             // the ith character
             String ros = str.substring(0, i) +
                     str.substring(i + 1);
-
             // Recurvise call
             printPermutn(ros, ans + ch);
         }
     }
-
     // Driver code
     public static void main(String[] args) {
-        String s = "abcd";
-        printPermutn(s, "");
+        String s = "abc";
+//        printPermutn(s, "");
+      permutation(s);
+    }
+
+
+    // Option 2:
+    public static void permutation(String str) {
+        permutation("", str);
+    }
+
+    private static void permutation(String prefix, String str) {
+        int n = str.length();
+        if (n == 0) System.out.println(prefix);
+        else {
+            for (int i = 0; i < n; i++)
+                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
+        }
     }
 }
